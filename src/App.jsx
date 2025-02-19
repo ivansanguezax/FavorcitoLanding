ae1ad2d544e09d2a094658d33c5d7805f8fb672d
ivansanguezax <ivansanguezax@gmail.com>
2025-02-19 12:50:18 -0400

    changes for SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { MainLayout } from "./layout/MainLayout";
-import { FormStudentsLayout } from "./layout/FormStudentsLayout";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { MainLayout } from './layout/MainLayout';
+import { FormStudentsLayout } from './layout/FormStudentsLayout';
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />} />
-        <Route path="/register/*" element={<FormStudentsLayout />} />
-      </Routes>
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainLayout />} />
+          <Route path="/register/*" element={<FormStudentsLayout />} />
+        </Routes>
+      </Router>
+    </HelmetProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-// main.jsx
-import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
-import "./index.css";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HelmetProvider>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </HelmetProvider>
-);
+  </StrictMode>,
+)
